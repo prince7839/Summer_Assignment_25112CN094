c486808b2650b5e8b74a1f2de712f555d6610007
PRINCE KUMAR YADAV <prince292yadav@gmail.com>
2026-06-25 00:11:03 +0530

    Day-24 assignment is uploaded

---
+++ Day-24/Find longest word.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[200], word[50], longest[50];
+    int i = 0, j = 0, max = 0, len;
+
+    printf("Enter sentence: ");
+    gets(str);
+
+    while(1)
+    {
+        if(str[i] != ' ' && str[i] != '\0')
+        {
+            word[j++] = str[i];
+        }
+        else
+        {
+            word[j] = '\0';
+
+            len = strlen(word);
+
+            if(len > max)
+            {
+                max = len;
+                strcpy(longest, word);
+            }
+
+            j = 0;
+        }
+
+        if(str[i] == '\0')
+            break;
+
+        i++;
+    }
+
+    printf("Longest word: %s", longest);
+
+    return 0;
+}
